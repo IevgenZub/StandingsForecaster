42cc033aee20ce667cd58971ab66a2c1d970d060
Ievgen-Zubkov <ievgen_zubkov@epam.com>
2019-02-04 21:43:01 +0200

    Add project files.

---
+++ StandingsForecaster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StandingsForecaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
